38f85d778c0e78d69eea44469499761eb1f840e2
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-01-12 23:58:12 -0500

    Add first version of extension code.

---
+++ index.js
@@ -0,0 +1,40 @@
+var gulp          = require('gulp'),
+    rename        = require('gulp-rename'),
+    svgSymbols    = require('gulp-svg-symbols'),
+    elixir        = require('laravel-elixir'),
+    notifications = require('laravel-elixir/ingredients/helpers/Notification'),
+    utilities     = require('laravel-elixir/ingredients/helpers/Utilities'),
+    _             = require('underscore')
+;
+
+elixir.extend('svgSymbols', function(options) {
+    var config = this,
+        defaultOptions = {
+            outputDir: 'public/svg',
+            rename: 'svg-symbols',
+            srcDir: config.assetsDir + 'svg'
+        }
+    ;
+
+    options = _.extend(defaultOptions, options);
+
+    gulp.task('svgSymbols', function() {
+
+        var onError = function(e) {
+            new notifications().error(e, 'SVG symbols sprite sheet creation failed');
+            this.emit('end');
+        };
+
+        return gulp.src(options.srcDir + '/**/*.svg')
+            .pipe(svgSymbols(options))
+            .on('error', onError)
+            .pipe(rename({ basename: options.rename }))
+            .pipe(gulp.dest(options.outputDir))
+            .pipe(new notifications().message('SVG symbols sprite sheet created on ' + options.outputDir));
+
+    });
+
+    this.registerWatcher('svgSymbols', options.srcDir + '/**/*.svg');
+
+    return this.queueTask('svgSymbols');
+});
